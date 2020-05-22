02c3681632e76388a37e31800f31cc23d9591bbe
Juan <juankorus@gmail.com>
2020-05-21 23:31:29 -0300

    Node component is animated

--- src/Components/Node.tsx
+++ src/Components/Node.tsx
@@ -1,43 +1,47 @@
-import React, { useState, useEffect, useLayoutEffect, useRef } from 'react';
+import React, { useRef, useMemo, useState, useEffect } from 'react';
 import { PQ_Node_json } from '../function/DS_PriorityQueue_BiHeap';
-
 export interface NodeProps {
     data: PQ_Node_json | undefined;
     isSmall: boolean;
-    index: number;
+    row: number;
 }
- 
-const Node: React.FC<NodeProps> = ({isSmall, data, index}) => {
 
-    const [showData, setData] = useState(data);
+const Node: React.FC<NodeProps> = ({data, isSmall, row}) => {
 
-    const refX = useRef<HTMLDivElement>(null);
+    const node = useMemo(()=>data!==undefined ? data : {val:-1,priority:0},[data]);
+    
+    const [currentAnim, setCurrentAnim] = useState<string>('BiHeap__Node--hide');
+    const [showStatus,  setshowStatus]  = useState<string>('BiHeap__Node--hidden');
+
+    const firstRun = useRef<boolean>(true);
 
     useEffect(()=> {
-        if(data?.priority!==undefined){
-            if(refX.current?.classList.contains('BiHeap__Node--hidden')){
-                refX.current?.classList.remove('BiHeap__Node--hidden');
-                refX.current?.classList.add('BiHeap__Node--show')
+        if (firstRun.current) {
+            if(node.priority){
+                console.log(node.priority)
+                setshowStatus('BiHeap__Node--visible');
+                setTimeout(() => {
+                    setCurrentAnim('BiHeap__Node--show');
+                }, 400)
+                firstRun.current = false;
+            }
+        }else{
+            setCurrentAnim('BiHeap__Node--hide');
+            if(node.priority){
+                setTimeout(() => {
+                    setCurrentAnim('BiHeap__Node--show')
+                }, 400)
             }
         }
-    },[data?.priority])
+    },[node.priority]);
 
-    useLayoutEffect(()=>{
-        if(data?.priority!==undefined){
-            refX.current?.classList.add('BiHeap__Node--show');
-        }else {
-            refX.current?.classList.add('BiHeap__Node--hidden');
-        }
-    },[])
-
-    return ( 
+    return (
         <div 
-            ref={refX}
-            className={`BiHeap__Node`}
-                >
-            {data!==undefined ? data.priority : ''}
+            // style={{...style}}
+            className={`BiHeap__Node ${currentAnim} ${showStatus}`}>
+            {data ? data.priority : ''}
         </div>
-    );
+    )
 }
 
 export default Node;    
