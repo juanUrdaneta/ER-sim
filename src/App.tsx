cfc00d37f80bca5f05e77850e7e866055c9844b9
Juan <juankorus@gmail.com>
2020-05-24 19:14:30 -0300

    A bit more clean up

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,17 @@ import PriorityQueue, { PQ_Node_json } from './function/DS_PriorityQueue_BiHeap'
 
 import './App.scss';
 
-const INTERVAL_DURATION = 800;
+const INTERVAL_DURATION = 1200;
+
+const DEFAULT_NODES = {
+  NODE_1: {val:'uno'   ,priority:1},
+  NODE_2: {val:'dos'   ,priority:2},
+  NODE_3: {val:'tres'  ,priority:3},
+  NODE_4: {val:'cuatro',priority:4},
+  NODE_5: {val:'cinco' ,priority:5},
+}
 
 const priQueue = new PriorityQueue();
-
 
 const App: React.FC = () => {
 
@@ -32,11 +39,11 @@ const App: React.FC = () => {
     <div className="container">
       <BiHeap list={queue} />
       <div className="lala" >
-        <button onClick={()=>handleInsert({val:'uno'   ,priority:1})}>INSERT 1</button>
-        <button onClick={()=>handleInsert({val:'dos'   ,priority:2})}>INSERT 2</button>
-        <button onClick={()=>handleInsert({val:'tres'  ,priority:3})}>INSERT 3</button>
-        <button onClick={()=>handleInsert({val:'cuatro',priority:4})}>INSERT 4</button>
-        <button onClick={()=>handleInsert({val:'cinco' ,priority:5})}>INSERT 5</button>
+        <button onClick={()=>handleInsert(DEFAULT_NODES.NODE_1)}>INSERT 1</button>
+        <button onClick={()=>handleInsert(DEFAULT_NODES.NODE_2)}>INSERT 2</button>
+        <button onClick={()=>handleInsert(DEFAULT_NODES.NODE_3)}>INSERT 3</button>
+        <button onClick={()=>handleInsert(DEFAULT_NODES.NODE_4)}>INSERT 4</button>
+        <button onClick={()=>handleInsert(DEFAULT_NODES.NODE_5)}>INSERT 5</button>
         <button onClick={()=>handleExtract()}>Extract</button>
       </div>
     </div>
