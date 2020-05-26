935c29f887ee67320a77a3c2c9e6ebe31dff7f59
Juan <juankorus@gmail.com>
2020-05-26 00:48:35 -0300

    Modal Working

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -11,15 +11,16 @@ const Modal: React.FC<ModalProps> = ({toggleModal}) => {
     const [CurrentAnimation, setCurrentAnimation] = useState<string>('');
     const [isFirstRun, setIsFirstRun] = useState<boolean>(true);
     const [isModal, setIsModal] = useState<boolean>(true);
-    const [isSmallModalVisible, setIsSmallModalVisible] = useState<boolean>(true);
+    const [isSmallModalVisible, setIsSmallModalVisible] = useState<boolean>(false);
     const [isShowing, setIsShowing] = useState<boolean>(true);
 
     const nextCard = () => {
         if(cardPage === 2 && isFirstRun) {
-            setCurrentAnimation('side-bar--hide-modal ');
+            setCurrentAnimation('side-bar--hide-modal');
             setCardPage(6);
             setIsModal(false);
             setTimeout(()=>{
+                setIsSmallModalVisible(true);
                 setCurrentAnimation('side-bar--show')
                 setCardPage(3);
             },600);
@@ -30,35 +31,69 @@ const Modal: React.FC<ModalProps> = ({toggleModal}) => {
             }, 200)
         } else {
             setIsFirstRun(false);
-            setIsSmallModalVisible(false);
+            setIsShowing(false);
             setTimeout(() => {
                 setCardPage(1);
             }, 300)
-            
         }
+    }
+
+    const skipCards = () => {
+        if (isModal) {
+            setIsFirstRun(false);
+            setCurrentAnimation('side-bar--hide-modal');
+            setTimeout(() => {
+                setIsModal(false);
+                setCardPage(1);
+            }, 300);
+        } else {
+            setIsFirstRun(false);
+            setIsSmallModalVisible(false);
+            setTimeout(() => {
+                setCardPage(1)
+            }, 300);
+        }
+    }
+
+    const toggleCard = () => {
+        setIsSmallModalVisible(!isSmallModalVisible);
+        setCurrentAnimation(isSmallModalVisible ? 'side-bar--hide' : 'side-bar--show')
     }
 
     useEffect(() => {
         setIsShowing(true);
     }, [cardPage]);
 
-    useEffect(()=>{
-        if (!isFirstRun) { 
-            setIsSmallModalVisible(!isSmallModalVisible);
-        }
-    },[toggleModal])
-
     return (
-        <div className={`side-bar ${CurrentAnimation} ${isSmallModalVisible ? '' : 'side-bar--hide'} ${isModal ? 'side-bar--is-modal': ''}`}>
-            <div className={`side-bar__content ${isShowing ? 'side-bar__content--show' : 'side-bar__content--hide'}`}>
+        <div className={`
+                side-bar 
+                ${CurrentAnimation} 
+                ${isSmallModalVisible ? '' : 'side-bar--hide'} 
+                ${isModal ? 'side-bar--is-modal': ''}`}>
+            <div className={
+                    `side-bar__content 
+                    ${isShowing ? 'side-bar__content--show' : 'side-bar__content--hide'}`}>
                 <h1 className='side-bar__title'>{MODAL_TITLES[cardPage]}</h1> 
-                <p className='side-bar__text'>{MODAL_TEXTS[cardPage]}</p>
+                <p  className='side-bar__text'> {MODAL_TEXTS[cardPage]}</p>
             </div>
             <div className="side-bar__next">
+                <p onClick={()=>skipCards()}>
+                    Skip
+                </p>
                 <span
                     onClick={()=>nextCard()} 
                     className="material-icons">
                     arrow_forward
+                </span>
+                
+            </div>
+            <div onClick={()=>toggleCard()}
+                 className={
+                     `toggle-side 
+                     ${isSmallModalVisible ? 'toggle-side--toggle': ''}
+                     ${isModal ? 'toggle-side--disabled' : ''}`}>
+                <span className="material-icons">
+                    help_outline
                 </span>
             </div>
         </div>
