3c865464e278b6c22b57d46d5a051df2a0227867
Juan <juankorus@gmail.com>
2020-05-27 23:13:05 -0300

    Added stages

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,14 @@ import Modal from './Components/Modal';
 import PriorityQueue, { PQ_Node_json } from './function/DS_PriorityQueue_BiHeap';
 
 import './App.scss';
-
 const INTERVAL_DURATION = 1200;
 
 const DEFAULT_NODES = {
-  NODE_1: {val:'uno'   ,priority:1},
-  NODE_2: {val:'dos'   ,priority:2},
-  NODE_3: {val:'tres'  ,priority:3},
-  NODE_4: {val:'cuatro',priority:4},
-  NODE_5: {val:'cinco' ,priority:5},
+  NODE_1: {val:'Cardiac Arrest' ,priority:1},
+  NODE_2: {val:'Asthma Attack' ,priority:2},
+  NODE_3: {val:'Abdominal Pain' ,priority:3},
+  NODE_4: {val:'Simple Laceration' ,priority:4},
+  NODE_5: {val:'Rash' ,priority:5},
 }
 
 const priQueue = new PriorityQueue();
@@ -20,7 +19,10 @@ const priQueue = new PriorityQueue();
 const App: React.FC = () => {
 
   const [queue, setQueue] = useState<Array<PQ_Node_json>>(priQueue.toArray());
-  const [enabled, setenabled] = useState(true)
+
+  const [enabled, setenabled] = useState(true);
+  const [isFirstRun, setisFirstRun] = useState(true);
+  const [tutorialIsAt, settutorialIsAt] = useState<number>(0);
 
   const handleInsert = (node: PQ_Node_json) => {
     setenabled(false);
@@ -45,36 +47,66 @@ const App: React.FC = () => {
   return (
     <div className="container">
       
-      <Modal toggleModal={true}/>
+      <Modal isFirstRun={isFirstRun} setIFR={setisFirstRun} setTIA={settutorialIsAt}/>
       <BiHeap list={queue} />
       <div className="action-menu" >
         <div 
-          className={`action-menu__button ${!enabled ? 'action-menu__button--disabled' : ''}`} 
+          className={`
+            action-menu__button 
+            ${!enabled ? 'action-menu__button--disabled' : ''}
+            ${tutorialIsAt === 3 ? 'action-menu__button--disabled' : ''}
+            ${tutorialIsAt === 4 ? 'action-menu__button--tutorial-show-this':''}
+            ${tutorialIsAt === 5 ? 'action-menu__button--disabled' : ''}`} 
           onClick={()=>handleInsert(DEFAULT_NODES.NODE_1)}>
-            INSERT 1
+            {DEFAULT_NODES.NODE_1.val}
         </div>
         <div 
-          className={`action-menu__button ${!enabled ? 'action-menu__button--disabled' : ''}`} 
+          className={`
+            action-menu__button 
+            ${!enabled ? 'action-menu__button--disabled' : ''}
+            ${tutorialIsAt === 3 ? 'action-menu__button--disabled' : ''}
+            ${tutorialIsAt === 4 ? 'action-menu__button--tutorial-show-this':''}
+            ${tutorialIsAt === 5 ? 'action-menu__button--disabled' : ''}`} 
           onClick={()=>handleInsert(DEFAULT_NODES.NODE_2)}>
-            INSERT 2
+            {DEFAULT_NODES.NODE_2.val}
         </div>
         <div 
-          className={`action-menu__button ${!enabled ? 'action-menu__button--disabled' : ''}`} 
+          className={`
+            action-menu__button 
+            ${!enabled ? 'action-menu__button--disabled' : ''}
+            ${tutorialIsAt === 3 ? 'action-menu__button--disabled' : ''}
+            ${tutorialIsAt === 4 ? 'action-menu__button--tutorial-show-this':''}
+            ${tutorialIsAt === 5 ? 'action-menu__button--disabled' : ''}`} 
           onClick={()=>handleInsert(DEFAULT_NODES.NODE_3)}>
-            INSERT 3
+            {DEFAULT_NODES.NODE_3.val}
         </div>
         <div 
-          className={`action-menu__button ${!enabled ? 'action-menu__button--disabled' : ''}`} 
+          className={`
+            action-menu__button 
+            ${!enabled ? 'action-menu__button--disabled' : ''}
+            ${tutorialIsAt === 3 ? 'action-menu__button--disabled' : ''}
+            ${tutorialIsAt === 4 ? 'action-menu__button--tutorial-show-this':''}
+            ${tutorialIsAt === 5 ? 'action-menu__button--disabled' : ''}`} 
           onClick={()=>handleInsert(DEFAULT_NODES.NODE_4)}>
-            INSERT 4
+            {DEFAULT_NODES.NODE_4.val}
         </div>
         <div 
-          className={`action-menu__button ${!enabled ? 'action-menu__button--disabled' : ''}`} 
+          className={`
+            action-menu__button 
+            ${!enabled ? 'action-menu__button--disabled' : ''}
+            ${tutorialIsAt === 3 ? 'action-menu__button--disabled' : ''}
+            ${tutorialIsAt === 4 ? 'action-menu__button--tutorial-show-this':''}
+            ${tutorialIsAt === 5 ? 'action-menu__button--disabled' : ''}`} 
           onClick={()=>handleInsert(DEFAULT_NODES.NODE_5)}>
-            INSERT 5
+            {DEFAULT_NODES.NODE_5.val}
         </div>
         <div 
-          className={`action-menu__button ${!enabled ? 'action-menu__button--disabled' : ''}`} 
+          className={`
+            action-menu__button 
+            ${!enabled ? 'action-menu__button--disabled' : ''}
+            ${tutorialIsAt === 3 ? 'action-menu__button--disabled' : ''}
+            ${tutorialIsAt === 4 ? 'action-menu__button--disabled' : ''}
+            ${tutorialIsAt === 5 ? 'action-menu__button--tutorial-show-this' : ''}`} 
           onClick={()=>handleExtract()}>Extract
         </div>
       
