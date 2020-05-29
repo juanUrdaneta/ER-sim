a6b4912d57f3c0a4dfa4915640396120cb4c34cd
Juan <juankorus@gmail.com>
2020-05-29 13:33:09 -0300

    fixed bug where extract will froce to reload site

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Modal from './Components/Modal';
 import PriorityQueue, { PQ_Node_json } from './function/DS_PriorityQueue_BiHeap';
 
 import './App.scss';
+import { DEFAULT_MIN_VERSION } from 'tls';
 const INTERVAL_DURATION = 1200;
 
 const DEFAULT_NODES = {
@@ -13,6 +14,21 @@ const DEFAULT_NODES = {
   NODE_4: {val:'Simple Laceration' ,priority:4},
   NODE_5: {val:'Rash' ,priority:5},
 }
+
+const pq = new PriorityQueue();
+pq.insert('uno',1);
+pq.insert('dos',2);
+pq.insert('tres',3);
+pq.insert('cuatro',4);
+pq.insert('cinco',5);
+
+pq.extractMax();
+console.log(pq.toArray());
+pq.extractMax();
+console.log(pq.toArray());
+pq.extractMax();
+console.log(pq.toArray());
+
 
 const priQueue = new PriorityQueue();
 
@@ -25,23 +41,36 @@ const App: React.FC = () => {
   const [tutorialIsAt, settutorialIsAt] = useState<number>(0);
 
   const handleInsert = (node: PQ_Node_json) => {
-    setenabled(false);
-    let {stop} = priQueue.insertWB(node.val,node.priority);
-    setQueue(priQueue.toArray());
-    setTimeout(() => {
-      if(!stop) return handleInsert(node);
-    }, INTERVAL_DURATION);
-    if (stop) {setenabled(true)}
+    if (!queue[0].priority) {
+      priQueue.insert(node.val,node.priority);
+      setQueue(priQueue.toArray());
+    } else {
+      setenabled(false);
+      let {stop} = priQueue.insertWB(node.val,node.priority);
+      setQueue(priQueue.toArray());
+      let x = setTimeout(() => {
+        if(!stop) return handleInsert(node);
+      }, INTERVAL_DURATION);
+      if (stop) {
+        clearTimeout(x);
+        setenabled(true)
+      }
+    }
   }
 
   const handleExtract = () => {
-    setenabled(false)
-    let {stop} = priQueue.extractMaxWB();
-    setQueue(priQueue.toArray());
-    setTimeout(() => {
-      if(!stop) return handleExtract();
-    }, INTERVAL_DURATION);
-    if (stop) {setenabled(true)}
+    if (queue[0].priority) {
+      setenabled(false)
+      let {stop} = priQueue.extractMaxWB();
+      setQueue(priQueue.toArray());
+      let x = setTimeout(() => {
+        if(!stop) return handleExtract();
+      }, INTERVAL_DURATION);
+      if (stop) {
+        setenabled(true);
+        clearTimeout(x);
+      }
+    } 
   }
 
   return (
